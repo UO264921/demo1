78a6883d775528763b0fe2a804c2ade2571ccd6c
uo264921 <uo264921@uniovi.es>
2020-09-25 13:00:02 +0200

    Primer guardado

---
+++ src/prueba1/Carrito.java
@@ -0,0 +1,27 @@
+package prueba1;
+
+public class Carrito {
+	private String[] pedido;
+	private double precio;
+	
+	public Carrito(String[] pedidoP) {
+		this.setPedido(pedidoP);
+		setPrecio(0);
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String[] getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(String[] pedido) {
+		this.pedido = pedido;
+	}
+}
