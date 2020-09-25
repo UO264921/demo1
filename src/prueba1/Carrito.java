5d932ec6bad4ddb0484be09a3fc99f6abea50d0d
uo264921 <uo264921@uniovi.es>
2020-09-25 13:28:46 +0200

    Se elimina la variable Precio

--- src/prueba1/Carrito.java
+++ src/prueba1/Carrito.java
@@ -2,19 +2,9 @@ package prueba1;
 
 public class Carrito {
 	private String[] pedido;
-	private double precio;
 	
 	public Carrito(String[] pedidoP) {
 		this.setPedido(pedidoP);
-		setPrecio(0);
-	}
-
-	public double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
 	}
 
 	public String[] getPedido() {
